1debd5ecbbed27f1f78ec37da735d60905920f90
spookeyy <pangasmeshack@gmail.com>
2024-07-01 21:29:02 +0300

    models

--- server/models.py
+++ server/models.py
@@ -10,7 +10,7 @@ db = SQLAlchemy(metadata=metadata)
 # define a model class by inheriting from db.Model.
 
 
-class Employee(db.Model):
+class Employee(db.Model): 
     __tablename__ = 'employees'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -19,3 +19,13 @@ class Employee(db.Model):
 
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.salary}>'
+
+class Department(db.Model):
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Department {self.id}, {self.name}>'
